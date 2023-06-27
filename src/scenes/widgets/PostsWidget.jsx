fe0cb91c2fa98bee3ea0167b5ee719ed517f9630
CristopherVA <ing.valentin99@hotmail.com>
2023-06-26 23:12:51 -0300

    done

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -19,7 +19,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `${process.env.REACT_APP_API_URL}posts/${userId}/posts`,
+      `${process.env.REACT_APP_API_URL}/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
